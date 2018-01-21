c762e4a57b7a601e6f8c2c4dfa189d27438d2504
Greg Eakin <greg@gdbtech.info>
2018-01-21 09:19:41 -0800

    New classes for TicTacToe

---
+++ GeneticAlgorithms/TicTacToe/SearchTimeoutException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace GeneticAlgorithms.TicTacToe
+{
+    public partial class Genetic<TGene, TFitness>
+        where TFitness : IComparable<TFitness>
+    {
+        public class SearchTimeoutException : Exception
+        {
+            public Chromosome<TGene, TFitness> Improvement { get; }
+
+            public SearchTimeoutException(Chromosome<TGene, TFitness> improvement)
+            {
+                Improvement = improvement;
+            }
+        }
+    }
+}
