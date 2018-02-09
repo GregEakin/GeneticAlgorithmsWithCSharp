3799f303e2f7e2045aa73516daf0e6b673732027
Greg Eakin <greg@gdbtech.info>
2018-02-09 06:45:28 -0800

    Collapse the Square creation logic into one switch.

--- GeneticAlgorithms/TicTacToe/Square.cs
+++ GeneticAlgorithms/TicTacToe/Square.cs
@@ -55,95 +55,93 @@ namespace GeneticAlgorithms.TicTacToe
             switch (index)
             {
                 case 1:
-                case 2:
-                case 3:
+                    Corner = true;
                     TopRow = true;
                     Row = new[] {1, 2, 3};
-                    break;
-                case 4:
-                case 5:
-                case 6:
-                    MiddleRow = true;
-                    Row = new[] {4, 5, 6};
-                    break;
-                case 7:
-                case 8:
-                case 9:
-                    BottomRow = true;
-                    Row = new[] {7, 8, 9};
-                    break;
-                default:
-                    throw new AccessViolationException();
-            }
-
-            switch (index % 3)
-            {
-                case 1:
                     LeftColumn = true;
                     Column = new[] {1, 4, 7};
+                    RowOpposite = 3;
+                    ColumnOpposite = 7;
+                    DiagonalOpposite = 9;
+                    Diagonals.Add(new[] { 1, 5, 9 });
                     break;
                 case 2:
+                    Side = true;
+                    TopRow = true;
+                    Row = new[] {1, 2, 3};
                     MiddleColumn = true;
                     Column = new[] {2, 5, 8};
+                    ColumnOpposite = 8;
                     break;
-                case 0:
+                case 3:
+                    Corner = true;
+                    TopRow = true;
+                    Row = new[] {1, 2, 3};
                     RightColumn = true;
                     Column = new[] {3, 6, 9};
+                    RowOpposite = 1;
+                    ColumnOpposite = 9;
+                    DiagonalOpposite = 7;
+                    Diagonals.Add(new[] { 7, 5, 3 });
+                    break;
+                case 4:
+                    Side = true;
+                    MiddleRow = true;
+                    Row = new[] {4, 5, 6};
+                    LeftColumn = true;
+                    Column = new[] {1, 4, 7};
+                    RowOpposite = 6;
+                    break;
+                case 5:
+                    Center = true;
+                    MiddleRow = true;
+                    Row = new[] {4, 5, 6};
+                    MiddleColumn = true;
+                    Column = new[] {2, 5, 8};
+                    Diagonals.Add(new[] { 1, 5, 9 });
+                    Diagonals.Add(new[] { 7, 5, 3 });
+                    break;
+                case 6:
+                    Side = true;
+                    MiddleRow = true;
+                    Row = new[] {4, 5, 6};
+                    RightColumn = true;
+                    Column = new[] {3, 6, 9};
+                    RowOpposite = 4;
+                    break;
+                case 7:
+                    Corner = true;
+                    BottomRow = true;
+                    Row = new[] {7, 8, 9};
+                    LeftColumn = true;
+                    Column = new[] {1, 4, 7};
+                    RowOpposite = 9;
+                    ColumnOpposite = 1;
+                    DiagonalOpposite = 3;
+                    Diagonals.Add(new[] { 7, 5, 3 });
+                    break;
+                case 8:
+                    Side = true;
+                    MiddleColumn = true;
+                    Column = new[] {2, 5, 8};
+                    BottomRow = true;
+                    Row = new[] {7, 8, 9};
+                    ColumnOpposite = 2;
+                    break;
+                case 9:
+                    Corner = true;
+                    BottomRow = true;
+                    Row = new[] {7, 8, 9};
+                    RightColumn = true;
+                    Column = new[] {3, 6, 9};
+                    RowOpposite = 7;
+                    ColumnOpposite = 3;
+                    DiagonalOpposite = 1;
+                    Diagonals.Add(new[] { 1, 5, 9 });
                     break;
                 default:
                     throw new AccessViolationException();
             }
-
-            if (index == 5)
-                Center = true;
-            else
-            {
-                if (index == 1 || index == 3 || index == 7 || index == 9)
-                    Corner = true;
-                else if (index == 2 || index == 4 || index == 6 || index == 8)
-                    Side = true;
-
-                switch (index)
-                {
-                    case 1:
-                        RowOpposite = 3;
-                        ColumnOpposite = 7;
-                        DiagonalOpposite = 9;
-                        break;
-                    case 2:
-                        ColumnOpposite = 8;
-                        break;
-                    case 3:
-                        RowOpposite = 1;
-                        ColumnOpposite = 9;
-                        DiagonalOpposite = 7;
-                        break;
-                    case 4:
-                        RowOpposite = 6;
-                        break;
-                    case 6:
-                        RowOpposite = 4;
-                        break;
-                    case 7:
-                        RowOpposite = 9;
-                        ColumnOpposite = 1;
-                        DiagonalOpposite = 3;
-                        break;
-                    case 8:
-                        ColumnOpposite = 2;
-                        break;
-                    case 9:
-                        RowOpposite = 7;
-                        ColumnOpposite = 3;
-                        DiagonalOpposite = 1;
-                        break;
-                }
-            }
-
-            if (index == 1 || DiagonalOpposite == 1 || Center)
-                Diagonals.Add(new[] {1, 5, 9});
-            if (index == 3 || DiagonalOpposite == 3 || Center)
-                Diagonals.Add(new[] {7, 5, 3});
         }
     }
 }
